03f234714eb178f74861369130313745006e8f88
Alvaro Tapia <ajtapia@miuandes.cl>
2026-04-26 16:29:32 -0400

    listo

--- consultas.py
+++ consultas.py
@@ -1,46 +1,95 @@
 import sys
 import json
 import pickle
+import time
 
-DATOS_FILE = "datos/datos.json"       # Cambiar a .pkl para probar ambos      
+DATA_TYPE = "pkl"         # Cambiar json/pkl para probar ambos
+DATOS_FILE = "datos/datos." + DATA_TYPE
 
-def max_vib_rango(params: list, out_file):
+def max_vib_rango(params: list, out_file, datos):
     sector = str(params[0])
     ts_ini = str(params[1])
     ts_fin = str(params[2])
-    result = ""
+    max_vib = None
+    for registro in datos["sectores"][sector]["VIB"]:
+        if registro[0] >= ts_ini and (max_vib is None or registro[1] > float(max_vib)):
+            max_vib = registro[1]
+        elif registro[0] > ts_fin:
+            break
+    if max_vib is None:
+        result = "NONE"
+    else:
+        result = f"{max_vib:.1f}"
     out_file.write(result + "\n")
 
-def prom_temp(params: list, out_file):
+def prom_temp(params: list, out_file, datos):
     sector = str(params[0])
     fecha = str(params[1])
-    result = ""
+    registros = []
+    for registro in datos["sectores"][sector]["TEMP"]:
+        if registro[0][:10] == fecha:
+            registros.append(registro[1])
+        elif registro[0][:10] > fecha:
+            break
+    if len(registros) > 0:
+        result = f"{(sum(registros) / len(registros)):.1f}"
+    else:
+        result = "NODATA"
     out_file.write(result + "\n")
 
-def picos_vib(params: list, out_file):
+def picos_vib(params: list, out_file, datos):
     sensor = str(params[0])
     umbral = float(params[1])
     timestamps = []
-    out_file.write(",".join(timestamps) + "\n")
+    for registro in datos["sensores"][sensor]["VIB"]:
+        if registro[1] > umbral:
+            timestamps.append(registro[0])
+    if len(timestamps) > 0:
+        out_file.write(",".join(timestamps) + "\n")
+    else:
+        out_file.write("NODATA\n")
 
-def rango_temp_ts(params: list, out_file):
+def rango_temp_ts(params: list, out_file, datos):
     sector = str(params[0])
     ts_ini = str(params[1])
     ts_fin = str(params[2])
-    result = ""
+    registros = []
+    for registro in datos["sectores"][sector]["TEMP"]:
+        if registro[0] >= ts_ini:
+            registros.append(registro[1])
+        elif registro[0] > ts_fin:
+            break
+    if len(registros) > 0:
+        result = f"{min(registros):.1f},{max(registros):.1f},{(sum(registros) / len(registros)):.1f}"
+    else:
+        result = "NODATA"
     out_file.write(result + "\n")
 
-def sensores_sector(params: list, out_file):
+def sensores_sector(params: list, out_file, datos):
     sector = str(params[0])
-    sensores = []
+    sensores = datos["sectores"][sector]["sensores"]
     sensores.sort(key=lambda x: int(x[1:]))
-    out_file.write(",".join(sensores) + "\n")
+    if len(sensores) > 0:
+        out_file.write(",".join(sensores) + "\n")
+    else:
+        out_file.write("NODATA\n")
 
-def siguiente_medicion(params: list, out_file):
+def siguiente_medicion(params: list, out_file, datos):
     sensor = str(params[0])
     tipo = str(params[1])
     timestamp = str(params[2])
-    result = ""
+    next_valid = False
+    for registro in datos["sensores"][sensor][tipo]:
+        if registro[0] == timestamp:
+            next_valid = True
+            continue
+        if next_valid:
+            res = registro
+            break
+    if res:
+        result = f"{res[0]},{res[1]:.1f}"
+    else:
+        result = "NONE"
     out_file.write(result + "\n")
 
 def clear_file(filename: str):
@@ -66,22 +115,30 @@ handlers = {
 }
 
 def main():
+    inicio = time.perf_counter()
+
     consultas_file = sys.argv[1]
     resultados_file = sys.argv[2]
 
-    clear_file(resultados_file)
+    datos = open_data(DATOS_FILE)
 
-    with open(resultados_file, "w") as out:
-        with open(consultas_file, "r") as f:
-            for line in f:
-                line_list = line.strip().split(" ")
-                consulta = line_list[0]
-                params = line_list[1:]
+    for _ in range(50):
+        clear_file(resultados_file)
 
-                handler = handlers.get(consulta)
-                if handler:
-                    handler(params, out) 
+        with open(resultados_file, "w") as out:
+            with open(consultas_file, "r") as f:
+                for line in f:
+                    line_list = line.strip().split(" ")
+                    consulta = line_list[0]
+                    params = line_list[1:]
+
+                    handler = handlers.get(consulta)
+                    if handler:
+                        handler(params, out, datos) 
             
+    fin = time.perf_counter()
+    tiempo_promedio = (fin - inicio) / 50
+    print(f"Tiempo usando {DATA_TYPE}: {tiempo_promedio * 1e3:.6f} milisegundos")
 
 if __name__ == "__main__":
     main()
---
+++ test.py
@@ -0,0 +1,13 @@
+import math
+
+ts1 = "2024-05-02T08:00:00"
+ts2 = "2025-05-01T12:00:00"
+
+lista = [1,2,3,4]
+
+for i in range(3):
+    if i == 2:
+        continue
+    print(i)
+
+#print(ts1[:10] > ts2[:10])
